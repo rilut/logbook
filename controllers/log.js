3637f305ccffa644d22f8229ffa612f0061319dc
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2017-10-10 09:04:07 +0700

    Edit button on logs datatable

--- controllers/log.js
+++ controllers/log.js
@@ -125,6 +125,10 @@ exports.deleteLog = (req, res, next) => {
 exports.getLogsDatatable = (req, res) => {
   req.query.populate = 'visitor';
   Log.dataTable(req.query, (err, data) => {
+    data.data = data.data.map((log) => {
+      log.actionEdit = `<a href="logs/${log._id}">Edit</a>`;
+      return log;
+    });
     res.send(data);
   });
 };

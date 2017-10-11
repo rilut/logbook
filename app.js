7eaf095f16c5437a929dec51fd81d9fd3e0ea785
Haydar Ali Ismail <haydar_ai@yahoo.com>
2017-10-11 21:00:48 +0800

    Add port option to sequelize initialization

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ mongoose.connection.on('error', (err) => {
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME,
   process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: process.env.MYSQL_DIALECT,
 
     pool: {
